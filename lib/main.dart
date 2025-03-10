e8c4e48b540e4406e18d03b1f97c9ea4f91da229
Steven Tran <steventran@Stevens-MacBook-Air.local>
2025-03-10 12:07:33 -0400

    Design UI includes cards.

--- lib/main.dart
+++ lib/main.dart
@@ -23,8 +23,42 @@ class CardMatchingScreen extends StatefulWidget {
 }
 
 class _CardMatchingScreenState extends State<CardMatchingScreen> {
+  List<Color> cardColors = [
+    Colors.red, Colors.red, 
+    Colors.green, Colors.green,
+    Colors.blue, Colors.blue,
+    Colors.yellow, Colors.yellow,
+    Colors.purple, Colors.purple,
+    Colors.orange, Colors.orange,
+    Colors.pink, Colors.pink,
+    Colors.teal, Colors.teal
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const CardMatchingScreen();
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Text('Card Matching Game', style: TextStyle(color: Colors.white)),
+        centerTitle: true,
+        backgroundColor: Colors.grey
+      ),
+      body: Padding (
+        padding: EdgeInsets.all(5),
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 4,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 30
+          ),
+          itemCount: 16,
+          itemBuilder: (context, index) {
+            return Card(
+              color: cardColors[index],
+            );
+          }
+        )
+      )
+    );
   }
 }
